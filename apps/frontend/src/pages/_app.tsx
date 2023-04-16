1dc195572e6fb58a5e5ab2d256470c8a1d53e2a7
miello <partearth1212@gmail.com>
2023-04-16 23:26:43 +0700

    feat: setup server-side theme and typography font

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
+import theme, { createEmotionCache } from "@/config/theme";
+import { CacheProvider } from "@emotion/react";
+import { EmotionCache } from "@emotion/cache";
 import type { AppProps } from "next/app";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface AppPropsWithCache extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App(props: AppPropsWithCache) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
