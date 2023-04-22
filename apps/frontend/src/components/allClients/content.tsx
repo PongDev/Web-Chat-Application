94836e4bcf53fee468a08fb9ccef1248b5b7a279
Pacharaponarp <71010056+Pacharaponarp@users.noreply.github.com>
2023-04-22 14:14:34 +0700

    feat: implement allClients page (#4)
    * feat: implement allClients page
    
    * fix: remove unneeded imports
    
    * feat: added title for allClients page

---
+++ apps/frontend/src/components/allClients/content.tsx
@@ -0,0 +1,45 @@
+import { Avatar, Stack, Typography } from "@mui/material";
+import React from "react";
+interface IClient {
+  userId: string;
+  displayName: string;
+  profileImage?: string;
+}
+const clients: IClient[] = [
+  { userId: "1", displayName: "aaaaaaaaaaa" },
+  {
+    userId: "2",
+    displayName: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbcsdsds",
+  },
+  { userId: "3", displayName: "cccc" },
+  { userId: "4", displayName: "d" },
+  { userId: "5", displayName: "eeeeeeeeeeeeeeeeeeee" },
+  { userId: "6", displayName: "ภาษาไทย" },
+  { userId: "7", displayName: "9595959efsffs" },
+];
+const Content = () => {
+  return (
+    <Stack direction="column" spacing={1} padding={8}>
+      <Typography variant="h4" component="span" paddingBottom={4}>
+        {"Client List"}
+      </Typography>
+      {clients.map((client) => (
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={2}
+          key={client.userId}
+        >
+          <Avatar
+            src={"https://cdn.myanimelist.net/images/characters/6/496453.jpg"}
+            sx={{ width: 52, height: 52 }}
+          />
+          <Typography variant="h5" component="span">
+            {client.displayName}
+          </Typography>
+        </Stack>
+      ))}
+    </Stack>
+  );
+};
+export default Content;
