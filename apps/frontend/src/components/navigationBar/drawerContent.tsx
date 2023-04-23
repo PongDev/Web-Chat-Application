9476182116cf68e53d50d21a8e8f36dfd377cce9
MinitrueSecretary <wipugeng@hotmail.com>
2023-04-24 00:33:57 +0700

    refactor: rename and remove imports

--- apps/frontend/src/components/navigationBar/drawerContent.tsx
+++ apps/frontend/src/components/navigationBar/drawerContent.tsx
@@ -1,27 +1,14 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Box, Button, InputAdornment, TextField } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import PublicIcon from "@mui/icons-material/Public";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-} from "@mui/material";
 
 function ItemGroups(groupName: String, items: String[]) {
   return (
