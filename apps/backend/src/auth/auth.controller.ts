a251c66f748f632afc40c58ba09b9ab433d61399
miello <partearth1212@gmail.com>
2023-04-24 14:31:57 +0700

    feat: implement refresh token endpoint

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, Injectable, Post, Req } from '@nestjs/common';
-import { GoogleTokenIDBody } from 'types';
+import { Body, Controller, Injectable, Post, UseGuards } from '@nestjs/common';
+import { GoogleTokenIDBody, JWTPayload } from 'types';
+import { JwtAuthGuard } from './jwt-refresh-auth.guard';
+import { User } from './user.decorator';
 
 @Controller('auth')
 @Injectable()
@@ -10,5 +12,11 @@ export class AuthController {
   @Post('/token/google')
   async googleLogin(@Body() body: GoogleTokenIDBody) {
     return this.authService.googleLogin(body.token);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/token/refresh')
+  async renewAccessToken(@User() user: JWTPayload) {
+    return this.authService.renewAccessToken(user);
   }
 }
