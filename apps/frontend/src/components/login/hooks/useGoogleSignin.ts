6000dd5b930d9ee59d78671aa06790b314c79e02
miello <partearth1212@gmail.com>
2023-04-24 14:52:53 +0700

    feat: connect auth to backend

--- apps/frontend/src/components/login/hooks/useGoogleSignin.ts
+++ apps/frontend/src/components/login/hooks/useGoogleSignin.ts
@@ -1,8 +1,10 @@
+import apiClient from "@/config/axios";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
+import { JWTToken } from "types";
 
 const useGoogleSignin = () => {
-  const history = useRouter();
+  const router = useRouter();
 
   const handleSignin = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.stopPropagation();
@@ -15,35 +17,42 @@ const useGoogleSignin = () => {
     }
   };
 
-  useEffect(() => {
-    const handleCallback = async (
-      response: google.accounts.id.CredentialResponse
-    ) => {
-      console.log(response.credential);
-    };
-
-    const initGoogleButton = async () => {
-      window.google.accounts.id.initialize({
-        client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
-        callback: handleCallback,
+  const handleCallback = useCallback(
+    async (response: google.accounts.id.CredentialResponse) => {
+      const res = await apiClient.post<JWTToken>("/auth/token/google", {
+        token: response.credential,
       });
-      window.google.accounts.id.renderButton(
-        document.getElementById("google_signin")!,
-        {
-          theme: "outline",
-          size: "large",
-          shape: "pill",
-          logo_alignment: "left",
-          type: "icon",
-        }
-      );
-      document
-        .getElementById("google_signin")
-        ?.getElementsByTagName("iframe")[0]
-        .remove();
-    };
+      localStorage.setItem("accessToken", res.data.accessToken);
+      localStorage.setItem("refreshToken", res.data.refreshToken);
+      router.push("/");
+    },
+    [router, apiClient]
+  );
+
+  const initGoogleButton = useCallback(async () => {
+    window.google.accounts.id.initialize({
+      client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
+      callback: handleCallback,
+    });
+    window.google.accounts.id.renderButton(
+      document.getElementById("google_signin")!,
+      {
+        theme: "outline",
+        size: "large",
+        shape: "pill",
+        logo_alignment: "left",
+        type: "icon",
+      }
+    );
+    document
+      .getElementById("google_signin")
+      ?.getElementsByTagName("iframe")[0]
+      .remove();
+  }, [handleCallback]);
+
+  useEffect(() => {
     initGoogleButton();
-  }, [history]);
+  }, [initGoogleButton]);
 
   return { handleSignin };
 };
