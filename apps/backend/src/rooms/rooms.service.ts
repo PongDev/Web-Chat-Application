a62a5f972983bd892409d68fff05cb294f75138a
miello <partearth1212@gmail.com>
2023-04-25 21:45:51 +0700

    feat: implement join and get room details

--- apps/backend/src/rooms/rooms.service.ts
+++ apps/backend/src/rooms/rooms.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateDMRoomBodyDto, CreateGroupRoomBodyDto, RoomType } from 'types';
 
@@ -27,13 +27,20 @@ export class RoomsService {
   }
 
   private async createNewDMRoom(userId1: string, userId2: string) {
+    if (userId1 === userId2)
+      throw new BadRequestException('Cannot create DM room with yourself');
+
+    if (userId1 > userId2) {
+      const temp = userId1;
+      userId1 = userId2;
+      userId2 = temp;
+    }
+
     return await this.prismaService.$transaction(async (tx) => {
       const room = await tx.directMessageRoom.findFirst({
         where: {
-          OR: [
-            { member1: userId1, member2: userId2 },
-            { member1: userId2, member2: userId1 },
-          ],
+          member1: userId1,
+          member2: userId2,
         },
       });
 
@@ -60,7 +67,61 @@ export class RoomsService {
     });
   }
 
+  private async getJoinedDMRoom(userId: string) {
+    const DMRoom = await this.prismaService.directMessageRoom.findMany({
+      where: {
+        OR: [
+          {
+            member1: userId,
+          },
+          {
+            member2: userId,
+          },
+        ],
+      },
+      select: {
+        id: true,
+        member1User: true,
+        member2User: true,
+      },
+    });
+
+    return DMRoom.map((room) => ({
+      id: room.id,
+      name:
+        room.member1User.id === userId
+          ? room.member2User.name
+          : room.member1User.name,
+    }));
+  }
+
+  private async getJoinedGroupRoom(userId: string) {
+    const groupRoom = await this.prismaService.groupRoomUser.findMany({
+      where: {
+        userId,
+      },
+      select: {
+        roomId: true,
+        groupRoom: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    return groupRoom.map((room) => ({
+      id: room.roomId,
+      name: room.groupRoom.name,
+    }));
+  }
+
   async getAllRooms(page: number, limit: number) {
+    if (!page) page = 1;
+    if (!limit) limit = 10;
+
+    const count = await this.prismaService.groupRoom.count();
+
     const skip = (page - 1) * limit;
     const rooms = await this.prismaService.groupRoom.findMany({
       skip,
@@ -77,12 +138,16 @@ export class RoomsService {
       },
     });
 
-    return rooms.map((room) => ({
-      id: room.id,
-      name: room.name,
-      owner: room.ownerUser.name,
-      users: room.GroupRoomUser.length,
-    }));
+    return {
+      page,
+      maxPage: Math.ceil(count / limit),
+      rooms: rooms.map((room) => ({
+        id: room.id,
+        name: room.name,
+        owner: room.ownerUser.name,
+        users: room.GroupRoomUser.length,
+      })),
+    };
   }
 
   async createNewRoom(
@@ -93,8 +158,10 @@ export class RoomsService {
       return await this.createNewGroupRoom(body.name, userId);
     }
     if (body.type === RoomType.DIRECT_MESSAGE_ROOM) {
-      return await this.createNewDMRoom(body.userID1, body.userID2);
+      return await this.createNewDMRoom(userId, body.userID);
     }
+
+    throw new BadRequestException('Invalid room type');
   }
 
   async getCreatedRooms(userId: string) {
@@ -109,5 +176,101 @@ export class RoomsService {
     });
 
     return rooms;
+  }
+
+  async getJoinedRooms(userId: string) {
+    const directRoom = await this.getJoinedDMRoom(userId);
+    const groupRoom = await this.getJoinedGroupRoom(userId);
+
+    return {
+      directRoom,
+      groupRoom,
+    };
+  }
+
+  async joinGroupRoom(roomId: string, userId: string) {
+    const room = await this.prismaService.groupRoom.findFirst({
+      where: {
+        id: roomId,
+      },
+    });
+
+    if (!room) throw new BadRequestException('Group Room not found');
+
+    await this.prismaService.groupRoomUser.upsert({
+      where: {
+        userId_roomId: {
+          userId,
+          roomId,
+        },
+      },
+      create: {
+        userId,
+        roomId,
+      },
+      update: {},
+    });
+
+    return {
+      id: room.id,
+    };
+  }
+
+  async getRoomInfo(roomId: string, userId: string) {
+    const room = await this.prismaService.room.findFirst({
+      where: {
+        id: roomId,
+      },
+      select: {
+        channelId: true,
+        GroupRoom: true,
+        privateRoom: true,
+      },
+    });
+
+    let name = '';
+
+    if (!room) throw new BadRequestException('Room not found');
+    if (room.GroupRoom) {
+      const groupRoom = await this.prismaService.groupRoom.findFirst({
+        where: {
+          id: roomId,
+        },
+        select: {
+          name: true,
+          ownerUser: {
+            select: {
+              name: true,
+            },
+          },
+          GroupRoomUser: true,
+        },
+      });
+
+      name = groupRoom.name;
+    }
+
+    if (room.privateRoom) {
+      const privateRoom = await this.prismaService.directMessageRoom.findFirst({
+        where: {
+          id: roomId,
+        },
+        select: {
+          member1User: true,
+          member2User: true,
+        },
+      });
+
+      name =
+        privateRoom.member1User.id === userId
+          ? privateRoom.member2User.name
+          : privateRoom.member1User.name;
+    }
+
+    return {
+      id: roomId,
+      name,
+      channelId: room.channelId,
+    };
   }
 }
