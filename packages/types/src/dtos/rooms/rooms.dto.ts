35a84f41b6f4053699ecd6405c9bea6f048cd066
miello <partearth1212@gmail.com>
2023-04-25 21:45:51 +0700

    feat: implement join and get room details

--- packages/types/src/dtos/rooms/rooms.dto.ts
+++ packages/types/src/dtos/rooms/rooms.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { Equals, IsEnum, IsString, ValidateNested } from "class-validator";
+import { Equals, IsString, ValidateNested } from "class-validator";
 
 export enum RoomType {
   DIRECT_MESSAGE_ROOM = "DIRECT_MESSAGE_ROOM",
@@ -11,25 +11,17 @@ export class CreateRoomTypeDto {
 }
 
 export class CreateDMRoomBodyDto {
-  @Equals(RoomType.DIRECT_MESSAGE_ROOM)
   type: RoomType.DIRECT_MESSAGE_ROOM;
 
   @IsString()
-  userID1: string;
-
-  @IsString()
-  userID2: string;
+  userID: string;
 }
 
 export class CreateGroupRoomBodyDto {
-  @Equals(RoomType.GROUP_ROOM)
   type: RoomType.GROUP_ROOM;
 
   @IsString()
   name: string;
-
-  @IsString()
-  ownerID: string;
 }
 
 export class CreateRoomDto {
@@ -40,11 +32,12 @@ export class CreateRoomDto {
       subTypes: [
         {
           value: CreateDMRoomBodyDto,
-          name: RoomType[RoomType.DIRECT_MESSAGE_ROOM],
+          name: RoomType.DIRECT_MESSAGE_ROOM,
         },
-        { value: CreateGroupRoomBodyDto, name: RoomType[RoomType.GROUP_ROOM] },
+        { value: CreateGroupRoomBodyDto, name: RoomType.GROUP_ROOM },
       ],
     },
+    keepDiscriminatorProperty: true,
   })
   body: CreateGroupRoomBodyDto | CreateDMRoomBodyDto;
 }
