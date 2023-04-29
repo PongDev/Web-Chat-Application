71c5ad42b2292162861b028ada5a8df6f27ae8b7
PongDev <pongwiwat.dev@gmail.com>
2023-04-29 21:07:58 +0700

    refactor: add api tags

--- apps/backend/src/rooms/rooms.module.ts
+++ apps/backend/src/rooms/rooms.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SocketService } from 'src/socket/socket.service';
 
 @Module({
   controllers: [RoomsController],
-  providers: [RoomsService, PrismaService],
+  providers: [RoomsService, PrismaService, SocketService],
 })
 export class RoomsModule {}
--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -3,23 +3,28 @@ import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from 'src/auth/user.decorator';
 import { JWTPayload, UpdateUserDto, UserDto } from 'types';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiBearerAuth()
   @Get()
   @UseGuards(JwtAuthGuard)
   async getAllUsers() {
     return await this.usersService.getAllUsers();
   }
 
+  @ApiBearerAuth()
   @Get('/me')
   @UseGuards(JwtAuthGuard)
   async getCurrentUser(@User() user: JWTPayload): Promise<UserDto> {
     return await this.usersService.getUserById(user.userID);
   }
 
+  @ApiBearerAuth()
   @Put('/me')
   @UseGuards(JwtAuthGuard)
   async updateCurrentUser(
