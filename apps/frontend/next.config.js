e90ff3f91989c3e3f91779da26ee78efd475f9c3
miello <partearth1212@gmail.com>
2023-04-30 22:51:56 +0700

    fix: nest error related

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
       transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
     },
   },
+  webpack: (config) => {
+    Object.assign(config.resolve.alias, {
+      "@nestjs/swagger": "frontend-optimizer",
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
