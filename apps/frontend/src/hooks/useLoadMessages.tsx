020385fa6b851b349829e33c04f233f5ecda26d8
miello <partearth1212@gmail.com>
2023-05-01 23:23:41 +0700

    fix: switch chat room does not fetch messages

--- apps/frontend/src/hooks/useLoadMessages.tsx
+++ apps/frontend/src/hooks/useLoadMessages.tsx
@@ -8,12 +8,12 @@ const useLoadMessages = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
   const [isBottom, setIsBottom] = useState<boolean>(false);
   const isLatest = useRef(true);
-  const memoIsLatest = useRef(false);
   const messageListRef = useRef<HTMLDivElement>(null);
   const [scrollHeight, setScrollHeight] = useState(0);
   const prevId = useRef("");
+  const isFirst = useRef(true);
   const router = useRouter();
-  const [isMore, setMore] = useState(true);
+  const isMore = useRef(true);
   const isSocketMessage = useRef(false);
 
   const handleSocketMessage = useCallback((message: string) => {
@@ -27,34 +27,39 @@ const useLoadMessages = () => {
       },
     ]);
 
-    memoIsLatest.current = isLatest.current;
     isSocketMessage.current = true;
   }, []);
 
-  const fetchMore = useCallback(async () => {
-    try {
-      if (!isMore) return;
+  const fetchMore = useCallback(
+    async (override?: boolean) => {
+      try {
+        if (!isMore.current && !override) return;
 
-      const query = new URLSearchParams();
-      if (prevId.current) query.append("prevMessageId", prevId.current);
-      query.append("limit", "15");
+        const query = new URLSearchParams();
+        if (prevId.current && !override)
+          query.append("prevMessageId", prevId.current);
+        query.append("limit", "15");
 
-      const response = await apiClient.get(
-        `messages/${router.query.id}?${query.toString()}`
-      );
+        const response = await apiClient.get(
+          `messages/${router.query.id}?${query.toString()}`
+        );
 
-      const messages = response.data;
-      messages.map((message: IMessage) => {
-        message.createdAt = dayjs(message.createdAt).format("YYYY-MM-DD HH:mm");
-      });
+        const messages = response.data.map((message: IMessage) => ({
+          ...message,
+          createdAt: dayjs(message.createdAt).format("YYYY-MM-DD HH:mm"),
+        }));
 
-      setMessages((prevMessages) => [...response.data, ...prevMessages]);
-      setMore(response.data.length > 0);
-      prevId.current = response.data[0].messageId;
-    } catch (error) {
-      console.error(error);
-    }
-  }, [isMore, router.query.id]);
+        if (override) setMessages(messages);
+        else setMessages((prevMessages) => [...messages, ...prevMessages]);
+
+        isMore.current = messages.length > 0;
+        prevId.current = response.data[0].messageId;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [router.query.id]
+  );
 
   useEffect(() => {
     messageListRef.current!.scrollTop = messageListRef.current!.scrollHeight;
@@ -89,33 +94,44 @@ const useLoadMessages = () => {
   }, []);
 
   useEffect(() => {
+    isFirst.current = true;
+    setScrollHeight(0);
+  }, [router.query.id]);
+
+  useEffect(() => {
+    if (router.isReady && isFirst.current) {
+      fetchMore(true);
+    }
+  }, [fetchMore, router.isReady, router.query.id]);
+
+  useEffect(() => {
     if (router.isReady) {
-      if (isBottom && isMore) {
+      if (!isFirst.current && isBottom && isMore.current) {
         fetchMore();
         setIsBottom(false);
       }
     }
-  }, [router, isBottom, isMore, fetchMore]);
+  }, [router, isBottom, fetchMore]);
 
   useEffect(() => {
-    if (
-      messageListRef.current &&
-      messageListRef.current.scrollHeight > scrollHeight &&
-      !isSocketMessage.current
-    ) {
-      messageListRef.current.scrollTop =
-        messageListRef.current.scrollHeight - scrollHeight;
+    if (!isFirst.current) {
+      if (
+        messageListRef.current &&
+        messageListRef.current.scrollHeight > scrollHeight &&
+        !isSocketMessage.current
+      ) {
+        messageListRef.current.scrollTop =
+          messageListRef.current.scrollHeight - scrollHeight;
+        setScrollHeight(messageListRef.current!.scrollHeight);
+      }
+    } else if (isFirst.current && messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
       setScrollHeight(messageListRef.current!.scrollHeight);
+      isFirst.current = false;
     }
+
     if (messages[0]) {
       prevId.current = messages[0].messageId;
-    }
-    if (
-      memoIsLatest.current &&
-      messageListRef.current &&
-      isSocketMessage.current
-    ) {
-      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [messages]);
